c8013ad8c6719a60c390bb0469618097e17538f1
Luca Butera <22855332+LucaButera@users.noreply.github.com>
2025-03-07 11:37:54 +0100

    added additional synch modes and updated pre-commit hooks

--- tsl/data/synch_mode.py
+++ tsl/data/synch_mode.py
@@ -5,9 +5,13 @@ class SynchMode(Enum):
     WINDOW = 'window'
     HORIZON = 'horizon'
     STATIC = 'static'
+    PAST = 'past'
+    HISTORY = 'history'
 
 
 # Aliases
 WINDOW = SynchMode.WINDOW
 HORIZON = SynchMode.HORIZON
 STATIC = SynchMode.STATIC
+PAST = SynchMode.PAST
+HISTORY = SynchMode.HISTORY
